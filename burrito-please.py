7c66128d3d2e1edad3b68cb51d5de3462601b053
Dennis <capone@alamon.com>
2021-04-06 15:50:18 -0600

    Get me that burrito

--- burrito-please.py
+++ burrito-please.py
@@ -4,6 +4,7 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+import time
 import os
 
 ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
@@ -13,25 +14,24 @@ bOptions.headless = True
 print("Sending request")
 
 
-
+# browser = webdriver.Firefox()
 browser = webdriver.Firefox(options = bOptions)
 browser.get("https://www.chipotle.com/contact-us")
+
+time.sleep(5)
 
 try:
     suggestion_box = WebDriverWait(browser,10).until(
         EC.presence_of_element_located((By.XPATH,'/html/body/div[2]/div/div[2]/div/div[4]/section/div/div/div/div[2]/div/div[3]'))
     )
-    print("Clicking box")
-    suggestion_box.click()
-    print("Box Clicked")
-
-
-
-
     suggestion_input = WebDriverWait(browser,10).until(
         EC.presence_of_element_located((By.XPATH,'/html/body/div[2]/div/div[2]/div/div[4]/section/div/div/div/div[5]/section/div[2]/div/div/div/div[4]/div/div[2]/form/div/input[1]'))
     )
     
+
+    print("Clicking box")
+    suggestion_box.click()
+    print("Box Clicked")
     suggestion_input.click()
     print("Entering 59901")
     suggestion_input.send_keys("59901")
